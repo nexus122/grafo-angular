5b96824d697965a9d3dca6eead22c4c4838e3a04
nexus122 <juan-pablo122@hotmail.es>
2024-11-18 18:15:10 +0100

    [instrucciones] feat: añadir una pagina de instrucciones para saber usar la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { InstructionsComponent } from './components/instructions/instructions.component';
+import { GrafoComponent } from './components/grafo/grafo.component';
+export const routes: Routes = [
+  { path: '', component: GrafoComponent },
+  { path: 'instructions', component: InstructionsComponent },
+];
