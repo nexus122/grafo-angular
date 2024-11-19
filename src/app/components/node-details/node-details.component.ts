bdb3ef8e46899c712b05054dc347a3e9bfd3cba5
nexus122 <juan-pablo122@hotmail.es>
2024-11-20 00:45:42 +0100

    [refactor-delete-modal] feat: Que la información de el nodo se cargue en la misma pagina en lugar de en una externa.

--- src/app/components/node-details/node-details.component.ts
+++ src/app/components/node-details/node-details.component.ts
@@ -1,30 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MarkdownModule } from 'ngx-markdown';
 import { Node } from '../../models/grafo.models';
 import { NodesService } from '../../services/nodes/nodes.service';
-import { ActivatedRoute } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-node-details',
   standalone: true,
-  imports: [CommonModule, FormsModule, MarkdownModule],
+  imports: [CommonModule, FormsModule, MarkdownModule, NgIf],
   templateUrl: './node-details.component.html',
   styleUrls: ['./node-details.component.scss'],
 })
-export class NodeDetailsComponent implements OnInit {
-  node!: Node;
-
-  constructor(
-    private route: ActivatedRoute,
-    private nodeService: NodesService
-  ) {}
-
-  ngOnInit() {
-    const nodeId = this.route.snapshot.paramMap.get('id');
-    this.node = this.nodeService.getNodeById(nodeId || '');
-  }
+export class NodeDetailsComponent {
+  @Input() node!: Node; // Asegurarse de que la propiedad esté decorada con @Input
+  constructor(private nodeService: NodesService) {}
 
   updateNode() {
     this.nodeService.updateNode(this.node);
@@ -32,5 +23,10 @@ export class NodeDetailsComponent implements OnInit {
 
   deleteNode() {
     this.nodeService.deleteNode(this.node.id);
+    this.closeScreen();
+  }
+
+  closeScreen() {
+    this.nodeService.selectedNode = null;
   }
 }
